3e83f938584f9907c4d86a36d6555506f486a4f0
unknown <limamonteiro2010@hotmail.com>
2020-04-30 23:05:08 -0300

    aa

--- calculadora-javascript-master/func.js
+++ calculadora-javascript-master/func.js
@@ -15,11 +15,29 @@ function igual(){
 
 function verifica_exibe(sinal){
     let tela = document.getElementById('resultado').value
+<<<<<<< HEAD
+    let soma = tela.includes('+')
+    let sub = tela.includes('-')
+    let mult = tela.includes('*')
+    let div = tela.includes('/')
+    if(tela){
+        if(!soma){
+            if(!sub){
+                if(!mult){
+                    if(!div){
+                        document.getElementById('resultado').value = tela + sinal
+                    }
+                }
+            }
+        }       
+
+=======
     let n = tela.includes(sinal)
     if(tela){
         if(!n){
         document.getElementById('resultado').value = tela + sinal
         }    
+>>>>>>> 1788a0bcdc6e1a57fce8e4dd71550e1ed807f674
     }
 
 }
